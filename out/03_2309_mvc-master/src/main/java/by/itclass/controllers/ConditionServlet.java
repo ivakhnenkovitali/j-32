672dcd275164cf99a38374eef8b771bedd3bc100
Asus <vaxa2320@gmail.com>
2023-06-13 18:22:41 +0300

    fpv_4

--- out/03_2309_mvc-master/src/main/java/by/itclass/controllers/ConditionServlet.java
+++ out/03_2309_mvc-master/src/main/java/by/itclass/controllers/ConditionServlet.java
@@ -1,5 +1,4 @@
 package by.itclass.controllers;
-
 import by.itclass.model.services.UserService;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
@@ -7,18 +6,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
-
 @WebServlet("/condition")
 public class ConditionServlet extends HttpServlet {
     private UserService service;
-
     @Override
     public void init() throws ServletException {
         service = new UserService();
     }
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doPost(req, resp);
