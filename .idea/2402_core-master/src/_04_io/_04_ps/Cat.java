12e7c643e14a18a78b216ecb93a5e4f58b74b0fa
Asus <vaxa2320@gmail.com>
2023-06-21 18:16:22 +0300

    fpv_4

--- .idea/2402_core-master/src/_04_io/_04_ps/Cat.java
+++ .idea/2402_core-master/src/_04_io/_04_ps/Cat.java
@@ -1,10 +1,7 @@
 package _04_io._04_ps;
-
 import lombok.AllArgsConstructor;
 import lombok.ToString;
-
 import java.io.Serializable;
-
 @AllArgsConstructor
 @ToString
 public class Cat implements Serializable {
