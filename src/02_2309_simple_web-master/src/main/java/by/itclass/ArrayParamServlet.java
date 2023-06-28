980771bcf13b5fd47afa8e8f441f8b13dffa38c7
Asus <vaxa2320@gmail.com>
2023-06-28 16:46:20 +0300

    fpv1

--- src/02_2309_simple_web-master/src/main/java/by/itclass/ArrayParamServlet.java
+++ src/02_2309_simple_web-master/src/main/java/by/itclass/ArrayParamServlet.java
@@ -3,11 +3,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
-
 @WebServlet("/arrayParam")
 public class ArrayParamServlet extends HttpServlet {
     @Override
