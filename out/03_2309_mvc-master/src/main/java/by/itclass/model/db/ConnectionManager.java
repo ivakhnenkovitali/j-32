33a1a0b2ff79a6b3d656a1c875b4974bbc5906a7
Asus <vaxa2320@gmail.com>
2024-11-15 20:16:56 +0300

    fpv_4

--- out/03_2309_mvc-master/src/main/java/by/itclass/model/db/ConnectionManager.java
+++ out/03_2309_mvc-master/src/main/java/by/itclass/model/db/ConnectionManager.java
@@ -1,14 +1,10 @@
 package by.itclass.model.db;
-
 import lombok.SneakyThrows;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-
 public class ConnectionManager {
     private static Connection cn;
     public static final String URL = "jdbc:mysql://localhost:3306/po_2309";
-
     @SneakyThrows
     public static void init() {
         Class.forName("com.mysql.cj.jdbc.Driver");
